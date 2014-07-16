164b1f106ac4c8a8ab2a981db69624c4cefad253
kyle-sablan <ksablan@rohan.sdsu.edu>
2014-07-16 14:18:37 -0700

    Confusion Matrix
    ...

---
+++ Output Stuff.R
@@ -0,0 +1,22 @@
+#Confusion Matrix Stuff
+#Select the the ones that you want to output
+#Add "$table" to only see the matrix
+#Add "$overall" to see the accuracy
+
+##Ones
+#confusionMatrix(texp$one$consl, texp$one$compl)
+#confusionMatrix(train$one$consl, train$one$compl)
+#confusionMatrix(test$one$consl, test$one$compl)
+#confusionMatrix(valid$one$consl, valid$one$compl)
+
+##Threes
+#confusionMatrix(texp$three$consl, texp$three$compl)
+#confusionMatrix(train$three$consl, train$three$compl)
+#confusionMatrix(test$three$consl, test$three$compl)
+#confusionMatrix(valid$three$consl, valid$three$compl)
+
+##Multi
+#confusionMatrix(texp$consl, texp$compl)
+#confusionMatrix(train$consl, train$compl)
+#confusionMatrix(test$consl, test$compl)
+#confusionMatrix(valid$consl, valid$compl)
