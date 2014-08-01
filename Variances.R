1362f0d3ea0a9089dbf9da926e0274cd6adac53f
Amelia <brunerson@gmail.com>
2014-08-01 12:53:37 -0500

    Idk

--- Variances.R
+++ Variances.R
@@ -1,3 +1,6 @@
 meanLabel = (data["Malignancy_1"]+data["Malignancy_2"]+data["Malignancy_3"]+data["Malignancy_4"])/4
 mvar = ((data["Malignancy_1"]-meanLabel)^2+(data["Malignancy_2"]-meanLabel)^2+
   (data["Malignancy_3"]-meanLabel)^2+(data["Malignancy_4"]-meanLabel)^2)/4
+
+#Explanation for why M3 and M4 have same accuracies
+lapply(allaccs, function(x) length(which(x["M3"]==x["M4"])))
